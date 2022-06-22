cf12282b0beec6a141c35ad7203fa946f84a2681
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-06-22 23:31:46 +0530

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,8 +7,8 @@ export type TimeLeft = {
 
 export const calculateTimeLeft = () => {
   let year = new Date().getFullYear();
-  let month = new Date().getMonth() + 8;
-  let day = new Date().getDate() + 9;
+  let month = new Date().getMonth() + 7;
+  let day = new Date().getDate() + 10;
 
   const difference = +new Date(`${year}-${month}-${day}`) - +new Date();
 
