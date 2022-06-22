92cb7c83457d652b588b05d74c21b3cbda9d8294
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-06-22 22:47:08 +0530

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -16,7 +16,7 @@ export const calculateTimeLeft = () => {
 
   if (difference > 0) {
     timeLeft = {
-      days: Math.floor(difference / (2000 * 60 * 60 * 24)),
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
       hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
       minutes: Math.floor((difference / 1000 / 60) % 60),
       seconds: Math.floor((difference / 1000) % 60),
